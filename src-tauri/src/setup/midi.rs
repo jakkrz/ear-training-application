60aa46317302c1fc0d45ba241c33cacc0229c582
jakkrz <jkranz.business@gmail.com>
2024-02-23 16:12:28 +0100

    implemented midi event firing from backend

--- src-tauri/src/setup/midi.rs
+++ src-tauri/src/setup/midi.rs
@@ -7,10 +7,24 @@ use midir::{Ignore, MidiInput};
 use tauri::{AppHandle, Manager};
 use tracing::info;
 
+#[derive(Clone, serde::Serialize)]
+struct NoteOnPayload {
+    channel: u8,
+    pitch: u8,
+    velocity: u8
+}
+
+#[derive(Clone, serde::Serialize)]
+struct NoteOffPayload {
+    channel: u8,
+    pitch: u8,
+    velocity: u8
+}
+
 pub fn setup(app: AppHandle) -> Result<(), ()> {
     info!("setting up midi");
     let mut midi_in = MidiInput::new("midir reading input").unwrap();
-    midi_in.ignore(Ignore::None);
+    midi_in.ignore(Ignore::All);
 
     // Get an input port (read from console if multiple are available)
     let in_ports = midi_in.ports();
@@ -43,12 +57,38 @@ pub fn setup(app: AppHandle) -> Result<(), ()> {
     let in_port_name = midi_in.port_name(in_port).unwrap();
 
     info!("Connection open, reading input from '{}'", in_port_name);
+
+    let new_app = app.clone();
+
     let conn_in = midi_in
         .connect(
             in_port,
             "midir-read-input",
             move |stamp, message, _| {
-                println!("{}: {:?} (len = {})", stamp, message, message.len());
+                info!("{}: {:?} (len = {})", stamp, message, message.len());
+                new_app.emit_all("onmidi", message).expect("failed to emit onmidi event");
+
+                let status_byte = message[0];
+                
+                if status_byte >> 4 == 0x9 {
+                    new_app.emit_all(
+                        "onmidinoteon",
+                        NoteOnPayload{
+                            channel: status_byte & 0x0F,
+                            pitch: message[1],
+                            velocity: message[2]
+                        }
+                    ).expect("failed to emit onmidinoteon event");
+                } else if status_byte >> 4 == 0x8 {
+                    new_app.emit_all(
+                        "onmidinoteoff",
+                        NoteOffPayload{
+                            channel: status_byte & 0x0F,
+                            pitch: message[1],
+                            velocity: message[2]
+                        }
+                    ).expect("failed to emit onmidinoteoff event");
+                }
             },
             (),
         )
