f5055e85da62d63d345312be8c268769181a5cd6
Jakub Kranz <jkranz.business@gmail.com>
2024-02-24 20:20:24 +0100

    formatted code + fixed bugs

--- src/NoteRecognitionGame.tsx
+++ src/NoteRecognitionGame.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect, useRef } from "react";
-import { exit } from '@tauri-apps/api/process';
+import { exit } from "@tauri-apps/api/process";
 
-import Vex, { SVGContext, StaveNote, GhostNote, Voice, Formatter, NoteStruct, Accidental } from "vexflow";
+import Vex, {
+  SVGContext,
+  StaveNote,
+  GhostNote,
+  Voice,
+  Formatter,
+  NoteStruct,
+  Accidental,
+} from "vexflow";
 const { Renderer, Stave } = Vex.Flow;
 
 import { listen } from "@tauri-apps/api/event";
@@ -11,224 +19,295 @@ import { listen } from "@tauri-apps/api/event";
 type Note = string;
 
 const NOTE_SEQUENCE_LENGTH = 5;
-const TIME_BETWEEN_NOTES = 4000;
-const STARTING_NOTE = "C/4"
+const TIME_BETWEEN_NOTES = 3000;
+const TIME_BEFORE_FIRST_NOTE = 1000;
+const STARTING_NOTE = "C/4";
 
 function getRandomArbitrary(min: number, max: number) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
 }
 
 function generateNoteSequence(sequenceLength = NOTE_SEQUENCE_LENGTH): Note[] {
-    let result: Note[] = [STARTING_NOTE];
-    let prevNote = STARTING_NOTE;
+  let result: Note[] = [STARTING_NOTE];
+  let prevNote = STARTING_NOTE;
 
-    for (let i = 1; i < sequenceLength; i++) {
-        let prevNotePitch = noteStringToPitch(prevNote);
-        let lowerBound = Math.min(60, prevNotePitch - 4);
-        let upperBound = Math.min(81, prevNotePitch + 4)
-        let newPitch = getRandomArbitrary(lowerBound, upperBound);
-        let noteToAdd = pitchToNoteString(newPitch);
-        result.push(noteToAdd);
-        prevNote = noteToAdd;
-    }
-    
-    return result;
+  for (let i = 1; i < sequenceLength; i++) {
+    let prevNotePitch = noteStringToPitch(prevNote);
+    let lowerBound = Math.max(60, prevNotePitch - 4);
+    let upperBound = Math.min(81, prevNotePitch + 4);
+    let newPitch = Math.floor(getRandomArbitrary(lowerBound, upperBound));
+    let noteToAdd = pitchToNoteString(newPitch);
+    result.push(noteToAdd);
+    prevNote = noteToAdd;
+  }
+
+  return result;
 }
 
 interface NoteRecognitionStaveProps {
-    teacherNotes: Note[]
-    studentNotes: Note[]
-};
+  teacherNotes: Note[];
+  studentNotes: Note[];
+}
 
 function getAccidentalsFromNote(note: string): string {
-    let [baseNote, _octave] = note.split("/");
-    return baseNote.slice(1);
+  let [baseNote, _octave] = note.split("/");
+  return baseNote.slice(1);
 }
 
-function NoteRecognitionStave({ teacherNotes, studentNotes }: NoteRecognitionStaveProps) {
-    const outputDivRef = useRef<HTMLDivElement>(null);
-    let studentNotesPlayed = studentNotes.length;
+function NoteRecognitionStave({
+  teacherNotes,
+  studentNotes,
+}: NoteRecognitionStaveProps) {
+  const outputDivRef = useRef<HTMLDivElement>(null);
+  let studentNotesPlayed = studentNotes.length;
 
-    useEffect(() => {
-        const outputDiv = outputDivRef.current!;
-        const renderer = new Renderer(outputDiv, Renderer.Backends.SVG);
+  useEffect(() => {
+    const outputDiv = outputDivRef.current!;
+    const renderer = new Renderer(outputDiv, Renderer.Backends.SVG);
 
-        renderer.resize(500, 100);
-        const context = renderer.getContext() as SVGContext;
+    renderer.resize(500, 100);
+    const context = renderer.getContext() as SVGContext;
 
-        const stave = new Stave(0, 0, 495);
-        stave.addClef("treble").addTimeSignature(`${NOTE_SEQUENCE_LENGTH}/4`);
-        stave.setContext(context).draw();
+    const stave = new Stave(0, 0, 495);
+    stave.addClef("treble").addTimeSignature(`${NOTE_SEQUENCE_LENGTH}/4`);
+    stave.setContext(context).draw();
 
-        const teacherVoice = new Voice({ num_beats: NOTE_SEQUENCE_LENGTH, beat_value: 4 });
-        const teacherNoteStructs: NoteStruct[] = teacherNotes.map((note) => {
-            return { keys: [note], duration: "q" }
-        });
+    const teacherVoice = new Voice({
+      num_beats: NOTE_SEQUENCE_LENGTH,
+      beat_value: 4,
+    });
+    const teacherNoteStructs: NoteStruct[] = teacherNotes.map((note) => {
+      return { keys: [note], duration: "q" };
+    });
 
-        const teacherNotesToDraw = teacherNoteStructs.map((struct, index) => {
-            let note = index < studentNotesPlayed ? new StaveNote(struct) : new GhostNote(struct);
-            let accidentals = getAccidentalsFromNote(teacherNotes[index]);
+    const teacherNotesToDraw = teacherNoteStructs.map((struct, index) => {
+      let note =
+        index < studentNotesPlayed
+          ? new StaveNote(struct)
+          : new GhostNote(struct);
 
-            if (accidentals !== "") {
-                note.addModifier(new Accidental(accidentals));
-            }
+      return note;
+    });
 
-            return note;
-        })
+    teacherNotes.forEach((value, index) => {
+      let accidentals = getAccidentalsFromNote(value);
 
-        const studentVoice = new Voice({ num_beats: NOTE_SEQUENCE_LENGTH, beat_value: 4 });
-        const studentVoiceNoteStructs: NoteStruct[] = [...studentNotes.map((note) => {
-            return { keys: [note], duration: "q" }
-        }), ...Array(NOTE_SEQUENCE_LENGTH - studentNotesPlayed).fill({ duration: "q" })];
+      if (accidentals !== "") {
+        teacherNotesToDraw[index].addModifier(new Accidental(accidentals));
+      }
+    });
 
-        const studentNotesToDraw = studentVoiceNoteStructs.map((struct, index) => {
-            let note = index < studentNotesPlayed ? new StaveNote(struct) : new GhostNote(struct);
+    const studentVoice = new Voice({
+      num_beats: NOTE_SEQUENCE_LENGTH,
+      beat_value: 4,
+    });
+    const studentVoiceNoteStructs: NoteStruct[] = [
+      ...studentNotes.map((note) => {
+        return { keys: [note], duration: "q" };
+      }),
+      ...Array(NOTE_SEQUENCE_LENGTH - studentNotesPlayed).fill({
+        duration: "q",
+      }),
+    ];
 
-            if (teacherNotes[index] == studentNotes[index]) {
-                note.setStyle({fillStyle: "green", strokeStyle: "green"});
-            } else {
-                note.setStyle({fillStyle: "red", strokeStyle: "red"});
-            }
+    const studentNotesToDraw = studentVoiceNoteStructs.map((struct, index) => {
+      let note =
+        index < studentNotesPlayed
+          ? new StaveNote(struct)
+          : new GhostNote(struct);
 
-            let accidentals = getAccidentalsFromNote(studentNotes[index]);            
+      if (teacherNotes[index] == studentNotes[index]) {
+        note.setStyle({ fillStyle: "green", strokeStyle: "green" });
+      } else {
+        note.setStyle({ fillStyle: "red", strokeStyle: "red" });
+      }
 
-            if (accidentals !== "") {
-                note.addModifier(new Accidental(accidentals));
-            }
+      return note;
+    });
 
-            return note;
-        })
+    studentNotes.forEach((value, index) => {
+      let accidentals = getAccidentalsFromNote(value);
 
-        teacherVoice.addTickables(teacherNotesToDraw);
-        studentVoice.addTickables(studentNotesToDraw);
+      if (accidentals !== "") {
+        studentNotesToDraw[index].addModifier(new Accidental(accidentals));
+      }
+    });
 
-        // the two voices are not "aware" of each other
-        new Formatter().joinVoices([studentVoice, teacherVoice]).format([studentVoice, teacherVoice], 445);
+    teacherVoice.addTickables(teacherNotesToDraw);
+    studentVoice.addTickables(studentNotesToDraw);
 
-        // Render voice
-        teacherVoice.draw(context, stave);
-        studentVoice.draw(context, stave);
+    // the two voices are not "aware" of each other
+    new Formatter()
+      .joinVoices([studentVoice, teacherVoice])
+      .format([studentVoice, teacherVoice], 445);
 
-        context.svg.style.height = "100%";
-        context.svg.style.width = "100%";
+    // Render voice
+    teacherVoice.draw(context, stave);
+    studentVoice.draw(context, stave);
 
-        return () => context.svg.remove();
-    }, [teacherNotes, studentNotes]);
+    context.svg.style.height = "100%";
+    context.svg.style.width = "100%";
 
-    return <div ref={outputDivRef} className="notation-container"></div>;
+    return () => context.svg.remove();
+  }, [teacherNotes, studentNotes]);
+
+  return <div ref={outputDivRef} className="notation-container"></div>;
 }
 
-const mapOffsetToNoteUsingSharps = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-const mapOffsetToNoteUsingFlats  = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
+const mapOffsetToNoteUsingSharps = [
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+  "A",
+  "A#",
+  "B",
+];
+const mapOffsetToNoteUsingFlats = [
+  "C",
+  "Db",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "Gb",
+  "G",
+  "Ab",
+  "A",
+  "Bb",
+  "B",
+];
 const USE_SHARPS = true;
 
 function noteStringsAreEquivalent(noteStringA: string, noteStringB: string) {
-    return noteStringToPitch(noteStringA) == noteStringToPitch(noteStringB);
+  return noteStringToPitch(noteStringA) == noteStringToPitch(noteStringB);
 }
 
 function pitchToNoteString(pitch: number): string {
-    let octave = Math.floor(pitch / 12);
-    let offset = pitch % 12
-    let noteName = "";
+  let octave = Math.floor(pitch / 12);
+  let offset = pitch % 12;
+  let noteName = "";
 
-    if (USE_SHARPS) {
-        noteName = mapOffsetToNoteUsingSharps[offset];
-    } else {
-        noteName = mapOffsetToNoteUsingFlats[offset];
-    }
-    
-    let noteString = `${noteName}/${octave - 1}`;
+  if (USE_SHARPS) {
+    noteName = mapOffsetToNoteUsingSharps[offset];
+  } else {
+    noteName = mapOffsetToNoteUsingFlats[offset];
+  }
 
-    return noteString;
+  let noteString = `${noteName}/${octave - 1}`;
+
+  return noteString;
 }
 
 function noteStringToPitch(noteString: string): number {
-    let [noteName, octaveString] = noteString.split("/");
-    let octave = Number(octaveString) + 1;
+  let [noteName, octaveString] = noteString.split("/");
+  let octave = Number(octaveString) + 1;
 
-    let offset = mapOffsetToNoteUsingSharps.indexOf(noteName);
-    offset = offset !== -1 ? offset : mapOffsetToNoteUsingFlats.indexOf(noteName);
+  let offset = mapOffsetToNoteUsingSharps.indexOf(noteName);
+  offset = offset !== -1 ? offset : mapOffsetToNoteUsingFlats.indexOf(noteName);
 
-    if (offset === -1) {
-        throw new Error(`Could not decode ${noteString} to pitch`);
-    }
+  if (offset === -1) {
+    throw new Error(`Could not decode ${noteString} to pitch`);
+  }
 
-    return octave * 12 + offset;
+  return octave * 12 + offset;
 }
 
 interface MidiNoteOnEvent {
-    payload: {
-        pitch: number,
-        velocity: number,
-        channel: number
-    }
-};
+  payload: {
+    pitch: number;
+    velocity: number;
+    channel: number;
+  };
+}
 
-export default function NoteRecognitionGame()  {
-    // const [gameState, setGameState] = useState<GameState>("PlayingSounds");
-    // const studentNotesPlayed = studentNotes.length;
-    // const menuActivated = studentNotesPlayed == NOTE_SEQUENCE_LENGTH;
+export default function NoteRecognitionGame() {
+  // const [gameState, setGameState] = useState<GameState>("PlayingSounds");
+  // const studentNotesPlayed = studentNotes.length;
+  // const menuActivated = studentNotesPlayed == NOTE_SEQUENCE_LENGTH;
 
-    const [teacherNotes, setTeacherNotes] = useState<Note[]>(["C#/4", "D/5", "A/4", "C/4", "D#/4"]);
-    const [studentNotes, setStudentNotes] = useState<Note[]>(["C/4", "C/4", "C/4", "C/4", "C/4"]);
-    const [acceptInput, setAcceptInput] = useState(false);
-    let menuActivated = studentNotes.length >= teacherNotes.length;
+  const [teacherNotes, setTeacherNotes] = useState<Note[]>(
+    generateNoteSequence()
+  );
+  const [studentNotes, setStudentNotes] = useState<Note[]>([]);
+  const [acceptInput, setAcceptInput] = useState(false);
+  let menuActivated = studentNotes.length >= teacherNotes.length;
 
-    useEffect(() => {
-        if (acceptInput) {
-            let unlistenPromise = listen("onmidinoteon", (event: MidiNoteOnEvent) => {
-                if (acceptInput && studentNotes.length < teacherNotes.length) {
-                    let pitch = event.payload.pitch;
-                    setStudentNotes((studentNotes) => [...studentNotes, pitchToNoteString(pitch)]);
-                }
-            })
+  useEffect(() => {
+    if (acceptInput) {
+      let studentNotesPlayed = studentNotes.length;
+      const teacherNotesPlayed = teacherNotes.length;
 
-            return () => {
-                unlistenPromise.then((unlisten) => unlisten());
-            };
-        } else {
-            let currentTimeoutId: ReturnType<typeof setTimeout> = -1;
-            let notesPlayed = 0;
-            function playNote() {
-                console.log(`Playing ${teacherNotes[notesPlayed]}`)
-                notesPlayed++;
-                if (notesPlayed < teacherNotes.length) {
-                    currentTimeoutId = setTimeout(playNote, TIME_BETWEEN_NOTES);
-                } else {
-                    setAcceptInput(true);
-                }
-            }
-            currentTimeoutId = setTimeout(playNote, TIME_BETWEEN_NOTES);
-
-            return () => {
-                clearTimeout(currentTimeoutId);
-            }
+      let unlistenPromise = listen("onmidinoteon", (event: MidiNoteOnEvent) => {
+        if (acceptInput && studentNotesPlayed < teacherNotesPlayed) {
+          console.log("yip");
+          let pitch = event.payload.pitch;
+          setStudentNotes((studentNotes) => [
+            ...studentNotes,
+            pitchToNoteString(pitch),
+          ]);
+          studentNotesPlayed++;
         }
-    }, [acceptInput]);
+      });
 
-    async function handleExitButtonClick() {
-        await exit(0);
-    }
-    function handleReplayButtonClick() {}
-    function handleNextButtonClick() {
-        setTeacherNotes(generateNoteSequence(NOTE_SEQUENCE_LENGTH));
-        setStudentNotes([]);
-        setAcceptInput(false);
-    }
+      return () => {
+        unlistenPromise.then((unlisten) => unlisten());
+      };
+    } else {
+      let currentTimeoutId: ReturnType<typeof setTimeout> = -1;
+      let notesPlayed = 0;
+      function playNote() {
+        console.log(`Playing ${teacherNotes[notesPlayed]}`);
+        notesPlayed++;
+        if (notesPlayed < teacherNotes.length) {
+          currentTimeoutId = setTimeout(playNote, TIME_BETWEEN_NOTES);
+        } else {
+          setAcceptInput(true);
+        }
+      }
+      currentTimeoutId = setTimeout(playNote, TIME_BEFORE_FIRST_NOTE);
 
-    return (
-        <div className="game-container">
-            <NoteRecognitionStave teacherNotes={teacherNotes} studentNotes={studentNotes}/>
-            <div className="button-container">
-                <button onClick={handleExitButtonClick}>Exit</button>
-                <button disabled={!menuActivated} onClick={handleReplayButtonClick}>Replay sound</button>
-                <button disabled={!menuActivated} onClick={handleNextButtonClick}>Next (press a note)</button>
-            </div>
-            <div className="points-container">
-                <span className="bold">Correct:</span> 0
-                <br/>
-                <span className="bold">Wrong:</span> 0
-            </div>
-        </div>
-    );
+      return () => {
+        clearTimeout(currentTimeoutId);
+      };
+    }
+  }, [acceptInput]);
+
+  async function handleExitButtonClick() {
+    await exit(0);
+  }
+  function handleReplayButtonClick() {}
+  function handleNextButtonClick() {
+    setTeacherNotes(generateNoteSequence(NOTE_SEQUENCE_LENGTH));
+    setStudentNotes([]);
+    setAcceptInput(false);
+  }
+
+  return (
+    <div className="game-container">
+      <NoteRecognitionStave
+        teacherNotes={teacherNotes}
+        studentNotes={studentNotes}
+      />
+      <div className="button-container">
+        <button onClick={handleExitButtonClick}>Exit</button>
+        <button disabled={!menuActivated} onClick={handleReplayButtonClick}>
+          Replay sound
+        </button>
+        <button disabled={!menuActivated} onClick={handleNextButtonClick}>
+          Next (press a note)
+        </button>
+      </div>
+      <div className="points-container">
+        <span className="bold">Correct:</span> 0
+        <br />
+        <span className="bold">Wrong:</span> 0
+      </div>
+    </div>
+  );
 }
