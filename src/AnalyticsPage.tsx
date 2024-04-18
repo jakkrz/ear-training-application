75139b74045ef731e8f1459ffee805c614f732b0
jakkrz <jkranz.business@gmail.com>
2024-04-18 18:24:48 +0200

    designed analytics page

--- src/AnalyticsPage.tsx
+++ src/AnalyticsPage.tsx
@@ -1,10 +1,26 @@
-import "./AnalyticsPage.css";
+import classes from "./AnalyticsPage.module.css";
 import { Link } from "react-router-dom";
+import { Collapsible, CollapsibleContainer } from "./Collapsible";
 
 export default function AnalyticsPage() {
     return (
-        <Link to={"/"} className="return-button">
-            back to home
-        </Link>
+        <>
+            <Link to={"/"} className="return-button">
+                back to home
+            </Link>
+            <div className={classes.containingFlex}>
+                <h1>Analytics</h1>
+                <div className={classes.scrollBox}>
+                    <CollapsibleContainer>
+                        <Collapsible text="Training-specific" depth={1}>
+                            <Collapsible text="More" depth={2}>
+                                askjdfkasjf
+                            </Collapsible>
+                        </Collapsible>
+                        <Collapsible text="General" depth={1}></Collapsible>
+                    </CollapsibleContainer>
+                </div>
+            </div>
+        </>
     );
 }
